7ed74241ef1af1f9eafe66a018219966dce140c5
oi <onno.invernizzi@ivention.nl>
2020-02-02 13:07:45 +0100

    Got something going here

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,33 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {  
-  entry: './js/index.js',
-  mode: "development",
-  devtool: "eval-source-map",
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    hot: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: './dist/index.html',
-      template: './dist/index.html'
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+let exclude = [path.resolve(__dirname, "dist")];
+
+module.exports = {
+    entry: './js/index.js',
+    mode: "development",
+    devtool: "source-map",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true,
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './index.html',
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+		rules: [
+			{ test: /\.ts$/, loader: "awesome-typescript-loader", exclude },
+			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader", exclude }
+		]
+	},
 };
