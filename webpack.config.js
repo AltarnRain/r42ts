452a5fb8806177362f97e06dedc37b597d8611d0
oi <onno.invernizzi@ivention.nl>
2020-03-22 15:13:48 +0100

    Moved JS output.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dist/index.js',
+    entry: './js/index.js',
     devtool: "source-map",
     output: {
         filename: 'bundle.js',
