9edd314d01e40a941122fe24c8cc0c6959efb6f2
oi <onno.invernizzi@ivention.nl>
2020-03-22 10:49:31 +0100

    Removed game actions double header.

--- src/Store/GameActions.ts
+++ src/Store/GameActions.ts
@@ -3,17 +3,6 @@
  * This source code is subject to terms and conditions.
  * See LICENSE.MD.
  */
-
-/**
- * Module:          GameActions
- * Responsibility:  Defines game actions
- */
-
-/**
- * @preserve Copyright 2010-2019 Onno Invernizzi.
- * This source code is subject to terms and conditions.
- * See LICENSE.MD.
- */
 /**
  * Module:          GameActions
  * Responsibility:  Defines game actions
