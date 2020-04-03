17dcb3d187f0bbaa5484bad6b2575ee6e213d82d
oi <onno.invernizzi@ivention.nl>
2020-04-03 19:37:03 +0200

    Phaser frame

---
+++ src/GameScreen/Phasers/PhaserFrame.ts
@@ -0,0 +1,21 @@
+/**
+ * @preserve Copyright 2019-2020 Onno Invernizzi.
+ * This source code is subject to terms and conditions.
+ * See LICENSE.MD.
+ */
+
+import { Frame } from "../../Types/Types";
+
+/**
+ * Module:          Phaser Frame
+ * Responsibility:  Frame for the phaser indicator
+ */
+
+const Phaser: Frame = [
+    ["E", "0"],
+    ["E", "0"],
+    ["0", "E"],
+    ["E", "E"],
+];
+
+export default Phaser;
