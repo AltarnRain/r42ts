2ecec89639e3d9ea9bdcb447ff4a16b25751b63d
oi <onno.invernizzi@ivention.nl>
2020-04-04 17:47:16 +0200

    Rename. Lives indicator is now a module

---
+++ src/GameScreen/Lives.ts
@@ -0,0 +1,66 @@
+/**
+ * @preserve Copyright 2019-2020 Onno Invernizzi.
+ * This source code is subject to terms and conditions.
+ * See LICENSE.MD.
+ */
+
+import CGAColors from "../Constants/CGAColors";
+import { PlayerFrames } from "../Player/PlayerFrames";
+import DimensionProvider from "../Providers/DimensionProvider";
+import renderFrame from "../Render/RenderFrame";
+import { getFrameDimensions, setFrameColor } from "../Utility/Frame";
+import { cloneObject } from "../Utility/Lib";
+
+/**
+ * Module:          Lives
+ * Responsibility:  Draws player lives
+ */
+
+// Clone the player frames so we can safely alter them.
+const lifeFrame = cloneObject(PlayerFrames);
+
+// Lives are completely yellow player ships
+setFrameColor(lifeFrame, CGAColors.yellow);
+
+const top = DimensionProvider().maxPixelSize;
+const leftStartPostion = DimensionProvider().fullWidth - DimensionProvider().maxPixelSize * 18;
+
+let lifeCount = 0;
+
+/**
+ * Sets the number of player lives.
+ * @param {number} lives. Player lives.
+ */
+export function setLives(value: number): void {
+    lifeCount = value;
+}
+
+/**
+ * Adds one life.
+ */
+export function addLife(): void {
+    lifeCount++;
+}
+
+/**
+ * Removes one life.
+ */
+export function removeLife(): void {
+    lifeCount--;
+}
+
+/**
+ * Draws the player lives.
+ */
+export function draw(): void {
+
+    let left = leftStartPostion;
+
+    // Start five game pixels from the right.
+    for (let lives = 1; lives <= 7; lives++) {
+        if (lives <= lifeCount) {
+            left = left - DimensionProvider().maxPixelSize * 2 - getFrameDimensions(lifeFrame).width;
+            renderFrame({ left, top }, lifeFrame);
+        }
+    }
+}
