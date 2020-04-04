a06739363a0eae61409b408a6a0999e2ec233d51
oi <onno.invernizzi@ivention.nl>
2020-04-04 17:47:48 +0200

    Runner is a module

--- src/Runner.ts
+++ src/Runner.ts
@@ -10,14 +10,11 @@
  */
 
 import BaseGameObject from "./Base/BaseGameObject";
-import Level from "./GameScreen/Level";
-import Lives from "./GameScreen/Lifes";
-import ScoreBoard from "./GameScreen/ScoreBoard";
 import { DrawGameField } from "./GameScreen/StaticRenders";
 import KeyboardState from "./Handlers/KeyboardStateHandler/KeyboardStateHandler";
-import IDraw from "./Interfaces/IDraw";
 import Explosion from "./Models/Explosion";
 import GameLocation from "./Models/GameLocation";
+import { Level, Lives, ScoreBoard } from "./Modules";
 import ExplosionCenter from "./Particles/ExplosionCenter";
 import Particle from "./Particles/Particle";
 import Player from "./Player/Player";
@@ -29,258 +26,217 @@ import { overlaps } from "./Utility/Lib";
 
 const fps = 1000 / 60;
 
-export default class Runner {
+/**
+ * Array of current game objects on screen.
+ */
+let enemies: BaseGameObject[] = [];
 
-    /**
-     * Array of current game objects on screen.
-     */
-    private enemies: BaseGameObject[] = [];
+/**
+ * Animation frame handler.
+ */
+let handler: number = 0;
 
-    /**
-     * Drawable objects like the lives indicator, score, etc.
-     */
-    private drawable: IDraw[] = [];
+/**
+ * Keeps track of the last tick when the animation was fired.
+ * Used to determine when to call the next frame.
+ */
+let lastTick: number = 0;
 
-    /**
-     * Animation frame handler.
-     */
-    private handler: number = 0;
+/**
+ * Reference to the player object.
+ */
+let player: BaseGameObject;
 
-    /**
-     * Keeps track of the last tick when the animation was fired.
-     * Used to determine when to call the next frame.
-     */
-    private lastTick: number = 0;
+/**
+ * Quick reference to the player bullet.
+ */
+let playerBullet: PlayerBullet | undefined;
 
-    /**
-     * Static reference to this class.
-     */
-    private static runner: Runner;
+/**
+ * Particles travelling on the screen.
+ */
+let particles: Particle[] = [];
 
-    /**
-     * Reference to the player object.
-     */
-    private player: BaseGameObject;
+/**
+ * Explosion centers on the screen.
+ */
+let explosionCenters: ExplosionCenter[] = [];
 
-    /**
-     * Quick reference to the player bullet.
-     */
-    private playerBullet: PlayerBullet | undefined;
+/**
+ * Start the runner.
+ */
+export function start(): void {
+    handler = window.requestAnimationFrame(run);
+}
 
-    /**
-     * Particles travelling on the screen.
-     */
-    private particles: Particle[] = [];
+/**
+ * Stop the runner.
+ */
+export function stop(): void {
+    window.cancelAnimationFrame(handler);
+}
 
-    /**
-     * Explosion centers on the screen.
-     */
-    private explosionCenters: ExplosionCenter[] = [];
+/**
+ * Runs the main game loop.
+ * @param {number} tick. The current tick.
+ */
+function run(tick: number): void {
+    // Runs all animation at the passed FPS
+    draw(tick);
 
-    private levelIndicator: Level;
+    // Self destruct
+    updateState();
 
-    private scoreBoard: ScoreBoard;
+    handler = window.requestAnimationFrame(run);
+}
 
-    private lives: Lives;
-
-    /**
-     * Constructs the Runner.
-     */
-    private constructor() {
-        this.run = this.run.bind(this);
-    }
-
-    /**
-     * Start the runner.
-     */
-    public start(): void {
-        this.handler = window.requestAnimationFrame(this.run);
-    }
-
-    /**
-     * Stop the runner.
-     */
-    public stop(): void {
-        window.cancelAnimationFrame(this.handler);
-    }
-
-    /**
-     * Runs the main game loop.
-     * @param {number} tick. The current tick.
-     */
-    private run(tick: number): void {
-        // Runs all animation at the passed FPS
-        this.draw(tick);
-
-        // Self destruct
-        this.updateState();
-
-        this.handler = window.requestAnimationFrame(this.run);
-    }
-
-    /**
-     * Called every request animation frame.
-     */
-    private updateState() {
-        if (KeyboardState.selfDestruct) {
-            if (this.enemies.length > 0 && this.player !== undefined) {
-                const assets = [
-                    ...this.enemies,
-                    this.player
-                ];
-                // Reset main rendering.
-                this.enemies = [];
-                this.player = undefined;
-                const explosionsLocations = assets.map((a) => explosionLocationProvider(a));
-                for (const explosionsLocation of explosionsLocations) {
-                    const center = new ExplosionCenter(explosionsLocation.explosion.frame, explosionsLocation.location, explosionsLocation.explosion.explosionCenterDelay);
-                    const particles = particleProvider(explosionsLocation.explosion, explosionsLocation.location);
-                    this.particles.push(...particles);
-                    this.explosionCenters.push(center);
-                }
+/**
+ * Called every request animation frame.
+ */
+function updateState() {
+    if (KeyboardState.selfDestruct) {
+        if (enemies.length > 0 && player !== undefined) {
+            const assets = [
+                ...enemies,
+                player
+            ];
+            // Reset main rendering.
+            enemies = [];
+            player = undefined;
+            const explosionsLocations = assets.map((a) => explosionLocationProvider(a));
+            for (const explosionsLocation of explosionsLocations) {
+                const center = new ExplosionCenter(explosionsLocation.explosion.frame, explosionsLocation.location, explosionsLocation.explosion.explosionCenterDelay);
+                const newParticles = particleProvider(explosionsLocation.explosion, explosionsLocation.location);
+                particles.push(...newParticles);
+                explosionCenters.push(center);
             }
         }
-        if (KeyboardState.fire && this.playerBullet === undefined && this.player !== undefined) {
-            this.playerBullet = new PlayerBullet(PlayerBulletFrame.F0, 270, 50, 1, this.player.getLocation());
-        }
-        if (this.player !== undefined || this.playerBullet !== undefined) {
-            const hittableObjects = [
-                ...this.enemies,
-                ...this.particles,
-                ...this.explosionCenters
-            ].filter((o) => o !== undefined);
-            if (hittableObjects.length > 0) {
-                for (const hittableObject of hittableObjects) {
-                    const type = hittableObject.getObjectType();
-                    switch (type) {
-                        case "explosion":
-                        case "particle":
-                        case "enemy": {
-                            // Check if player got hit.
-                            if (this.player !== undefined) {
-                                // Get all the locations of hittable objects and check if the player might be hit.
-                                const hittableObjectLocations = hittableObject.getLocations();
-                                const playerLocations = this.player.getLocations();
-                                const playerExplosion = this.player.getExplosion();
-                                const playerLocation = this.player.getLocation();
-                                hittableObjectLocations.forEach((hloc) => {
-                                    playerLocations.forEach((ploc) => {
-                                        const playerHit = overlaps(hloc, ploc);
-                                        if (playerHit) {
-                                            this.renderExplosion(playerExplosion, playerLocation);
-                                            this.player = undefined;
-                                            this.lives.removeLife();
-                                        }
-                                    });
+    }
+
+    if (KeyboardState.fire && playerBullet === undefined && player !== undefined) {
+        playerBullet = new PlayerBullet(PlayerBulletFrame.F0, 270, 50, 1, player.getLocation());
+    }
+
+    if (player !== undefined || playerBullet !== undefined) {
+        const hittableObjects = [
+            ...enemies,
+            ...particles,
+            ...explosionCenters
+        ].filter((o) => o !== undefined);
+
+        if (hittableObjects.length > 0) {
+            for (const hittableObject of hittableObjects) {
+                const type = hittableObject.getObjectType();
+                switch (type) {
+                    case "explosion":
+                    case "particle":
+                    case "enemy": {
+                        // Check if player got hit.
+                        if (player !== undefined) {
+                            // Get all the locations of hittable objects and check if the player might be hit.
+                            const hittableObjectLocations = hittableObject.getLocations();
+                            const playerLocations = player.getLocations();
+                            const playerExplosion = player.getExplosion();
+                            const playerLocation = player.getLocation();
+                            hittableObjectLocations.forEach((hloc) => {
+                                playerLocations.forEach((ploc) => {
+                                    const playerHit = overlaps(hloc, ploc);
+                                    if (playerHit) {
+                                        renderExplosion(playerExplosion, playerLocation);
+                                        player = undefined;
+                                        Lives.removeLife();
+                                    }
                                 });
-                            }
+                            });
                         }
-                        case "playerbullet": {
-                            if (this.playerBullet) {
-                                const hittableObjectLocations = hittableObject.getLocations();
-                                const playerBulletLocations = this.playerBullet.getLocations();
-                                hittableObjectLocations.forEach((hloc) => {
-                                    playerBulletLocations.forEach((ploc) => {
-                                        const hit = overlaps(hloc, ploc);
-                                        if (hit) {
-                                            this.playerBullet = undefined;
-                                            if (hittableObject.getObjectType() === "enemy") {
-                                                this.renderExplosion(hittableObject.getExplosion(), hittableObject.getLocation());
-                                                this.scoreBoard.addToScore(hittableObject.getPoints());
-                                                this.enemies = this.enemies.filter((e) => e !== hittableObject);
-                                            }
+                    }
+                    case "playerbullet": {
+                        if (playerBullet) {
+                            const hittableObjectLocations = hittableObject.getLocations();
+                            const playerBulletLocations = playerBullet.getLocations();
+                            hittableObjectLocations.forEach((hloc) => {
+                                playerBulletLocations.forEach((ploc) => {
+                                    const hit = overlaps(hloc, ploc);
+                                    if (hit) {
+                                        playerBullet = undefined;
+                                        if (hittableObject.getObjectType() === "enemy") {
+                                            renderExplosion(hittableObject.getExplosion(), hittableObject.getLocation());
+                                            ScoreBoard.addToScore(hittableObject.getPoints());
+                                            enemies = enemies.filter((e) => e !== hittableObject);
                                         }
-                                    });
+                                    }
                                 });
-                            }
+                            });
                         }
                     }
                 }
             }
         }
     }
+}
 
-    /**
-     * Called every request animation frame. Draws objects.
-     * @param {number} tick. Tick.
-     */
-    private draw(tick: number) {
-        if (tick - this.lastTick > fps) {
-            DrawGameField();
-            if (this.drawable) {
-                this.drawable.forEach((d) => d.draw(tick));
-            }
-            if (this.player) {
-                this.player.draw(tick);
-            }
-            this.enemies.forEach((go) => go.draw(tick));
-            if (this.particles.length > 0) {
-                this.particles.forEach((p) => p.draw(tick));
-                this.particles = this.particles.filter((p) => p.inScreen());
-            }
-            if (this.explosionCenters.length > 0) {
-                this.explosionCenters.forEach((ec) => ec.draw(tick));
-                this.explosionCenters = this.explosionCenters.filter((ec) => ec.fizzledOut());
-            }
-            // Bullet left the field.
-            if (this.playerBullet && !this.playerBullet.inScreen()) {
-                this.playerBullet = undefined;
-            }
-            if (this.playerBullet !== undefined) {
-                this.playerBullet.draw(tick);
-            }
-            this.lastTick = tick;
-        }
-    }
+/**
+ * Called every request animation frame. Draws objects.
+ * @param {number} tick. Tick.
+ */
+function draw(tick: number) {
+    if (tick - lastTick > fps) {
 
-    private renderExplosion(explosion: Explosion, location: GameLocation) {
-        const center = new ExplosionCenter(explosion.frame, location, explosion.explosionCenterDelay);
-        const particles = particleProvider(explosion, location);
-        this.particles.push(...particles);
-        this.explosionCenters.push(center);
-    }
+        DrawGameField();
 
-    /**
-     * Register an IDrawable class.
-     * @param drawable.
-     * @returns {() => void}. Function to remove the object from the array of drawable objects.
-     */
-    public register(gameObject: BaseGameObject): void {
-        this.enemies.push(gameObject);
-    }
+        Level.draw();
+        Lives.draw();
+        ScoreBoard.draw();
 
-    /**
-     * Registers the player.
-     * @param {Player} player. The player object.
-     */
-    public registerPlayer(player: Player): void {
-        this.player = player;
-    }
-
-    public registerLives(lives: Lives): void {
-        this.lives = lives;
-        this.drawable.push(lives);
-    }
-
-    public registerScore(scoreBoard: ScoreBoard): void {
-        this.scoreBoard = scoreBoard;
-        this.drawable.push(scoreBoard);
-    }
-
-    public registerLevelIndicator(levelIndicator: Level): void {
-        this.levelIndicator = levelIndicator;
-        this.drawable.push(levelIndicator);
-    }
-
-    /**
-     * Get the game runner instance.
-     * @returns {Runner}. Reference to the game runner.
-     */
-    public static get(): Runner {
-        if (!Runner.runner) {
-            Runner.runner = new Runner();
+        if (player) {
+            player.draw(tick);
         }
 
-        return Runner.runner;
+        enemies.forEach((go) => go.draw(tick));
+
+        if (particles.length > 0) {
+            particles.forEach((p) => p.draw(tick));
+            particles = particles.filter((p) => p.inScreen());
+        }
+
+        if (explosionCenters.length > 0) {
+            explosionCenters.forEach((ec) => ec.draw(tick));
+            explosionCenters = explosionCenters.filter((ec) => ec.fizzledOut());
+        }
+
+        // Bullet left the field.
+        if (playerBullet && !playerBullet.inScreen()) {
+            playerBullet = undefined;
+        }
+
+        if (playerBullet !== undefined) {
+            playerBullet.draw(tick);
+        }
+
+        lastTick = tick;
     }
+}
+
+function renderExplosion(explosion: Explosion, location: GameLocation) {
+    const center = new ExplosionCenter(explosion.frame, location, explosion.explosionCenterDelay);
+    const newParticles = particleProvider(explosion, location);
+    particles.push(...newParticles);
+    explosionCenters.push(center);
+}
+
+/**
+ * Register an IDrawable class.
+ * @param drawable.
+ * @returns {() => void}. Function to remove the object from the array of drawable objects.
+ */
+export function register(gameObject: BaseGameObject): void {
+    enemies.push(gameObject);
+}
+
+/**
+ * Registeres the player
+ * @param {Player} value. Player object.
+ */
+export function registerPlayer(value: Player) {
+    player = value;
 }
