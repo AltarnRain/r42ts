5a7626d51a902a6a4738830129c7c7d08f1674c2
oi <onno.invernizzi@ivention.nl>
2020-04-04 17:47:59 +0200

    Export all modules under a set ailas

---
+++ src/Modules.ts
@@ -0,0 +1,20 @@
+/**
+ * @preserve Copyright 2019-2020 Onno Invernizzi.
+ * This source code is subject to terms and conditions.
+ * See LICENSE.MD.
+ */
+
+/**
+ * Module:          Modules
+ * Responsibility:  Export modules with aliases.
+ */
+
+import * as level from "./GameScreen/Level";
+import * as lives from "./GameScreen/Lives";
+import * as scoreBord from "./GameScreen/ScoreBoard";
+import * as runner from "./Runner";
+
+export { runner as Runner };
+export { lives as Lives };
+export { scoreBord as ScoreBoard };
+export { level as Level };
