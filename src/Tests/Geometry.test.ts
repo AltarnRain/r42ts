451c9c12c823247268bc75f6853c447182d4909d
oi <onno.invernizzi@ivention.nl>
2020-04-14 20:26:25 +0200

    Fixed wrong angle issue when top or left differences are 0.

--- src/Tests/Geometry.test.ts
+++ src/Tests/Geometry.test.ts
@@ -9,6 +9,7 @@
  * Responsibility:  Tests Geometry functions
  */
 
+import GameLocation from "../Models/GameLocation";
 import * as Geometry from "../Utility/Geometry";
 
 describe("geometry tests", () => {
@@ -24,5 +25,31 @@ describe("geometry tests", () => {
         expect(a2).toBe(270);
         expect(a3).toBe(360);
         expect(a4).toBe(90);
+    });
+
+    test("calculateVector when the dy is 0, it should still return an angle", () => {
+        const sourceLocation: GameLocation = {
+            top: 0,
+            left: 0,
+        };
+
+        // Act
+        const angle1 = Geometry.calculateAngle(sourceLocation, { top: 0, left: 10 });
+
+        // Assert
+        expect(angle1).not.toBe(-0);
+    });
+
+    test("calculateVector when the dx is 0, it should still return an angle", () => {
+        const sourceLocation: GameLocation = {
+            top: 0,
+            left: 1,
+        };
+
+        // Act
+        const angle1 = Geometry.calculateAngle(sourceLocation, { top: 100, left: 1 });
+
+        // Assert
+        expect(angle1).not.toBe(-90);
     });
 });
