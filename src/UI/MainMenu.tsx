f54df32725c723dee0401abc1eb57ee8d4926320
oi <onno.invernizzi@ivention.nl>
2020-05-25 22:34:55 +0200

    Added button to github

--- src/UI/MainMenu.tsx
+++ src/UI/MainMenu.tsx
@@ -16,7 +16,7 @@ import { Styles } from "./Styles";
 
 export default function MainMenu(): JSX.Element {
 
-    const [screenState, setScreenState] = useState<"mainmenu" | "playing" | "about" | "gameover">("mainmenu");
+    const [screenState, setScreenState] = useState<"mainmenu" | "playing" | "about" | "gameover">("about");
     const [gameResult, setGameResult] = useState<GameResultModel>();
 
     /**
@@ -77,7 +77,7 @@ export default function MainMenu(): JSX.Element {
                             <br />
                             <div style={Styles.defaultContainer}>
                                 <div style={{ flexDirection: "column" }}>
-                                    <h2 style={Styles.textStyle}>Instructions</h2>
+                                    <p style={Styles.textStyle}>Instructions</p>
                                     <ul style={Styles.textStyle}>
                                         <li>Use the arrow keys to move.</li>
                                         <li>Press F1 to fire a bullet.</li>
@@ -112,17 +112,16 @@ export default function MainMenu(): JSX.Element {
                             </div>
                         </> :
                         screenState === "about" ?
-                            <>
-                                <HoverButton onClick={() => setScreenState("mainmenu")} text="Back to main menu" hoverStyle={Styles.buttonHoverStyle} normalStyle={Styles.buttonStyle} />
-                                <div style={Styles.textStyle}>
-                                    <h1 style={Styles.textStyle}>About me</h1>
-                                    <p>
+                            <div style={Styles.defaultContainer}>
+                                <p style={Styles.header}>About me</p>
+                                <div style={{flexDirection: "column"}} >
+                                    <p style={Styles.textStyle}>
                                         My name is Antonio Invernizzi. I've worked as a professional programmer for 20 years.<br />
                                         <br />
-                                    Round 42 is a big reason why.<br />
+                                        Round 42 is a big reason why.<br />
                                         <br />
-                                    It was 1990 and my parents bought our first PC. A 4 Mhz 8088 XT. It shipped with 3 games:
-                                    <ul>
+                                        It was 1990 and my parents bought our first PC. A 4 Mhz 8088 XT. It shipped with 3 games:
+                                        <ul>
                                             <li>Digger</li>
                                             <li>A pinball machine</li>
                                             <li>Round 42</li>
@@ -130,11 +129,11 @@ export default function MainMenu(): JSX.Element {
                                     </p>
                                     <p>
                                         It is fair to say I was instantly addicted to Round 42 and spend many, many hours trying to beat the game often with my mother watching. <br />
-                                    Good times :) <br />
-                                    Course, spending hour uppon hour behind a PC made me curious what else I could do with it and... well... now I'm a programmer. <br />
-                                    Though, I am not a game developer by trade I realy enjoyed the challenge of writing one.
-                                </p>
-                                    <h1 style={Styles.textStyle}>Technologies used</h1>
+                                        Good times :) <br />
+                                        Course, spending hour uppon hour behind a PC made me curious what else I could do with it and... well... now I'm a programmer. <br />
+                                        Though, I am not a game developer by trade I realy enjoyed the challenge of writing one.
+                                    </p>
+                                    <p style={Styles.header}>Technologies used</p>
                                     <ul>
                                         <li>TypeScript for coding</li>
                                         <li>WebPack for packaging</li>
@@ -142,9 +141,10 @@ export default function MainMenu(): JSX.Element {
                                         <li>Immer for QoL state management.</li>
                                         <li>React (menu only)</li>
                                         <li>Howler for playing sounds.</li>
-                                    </ul>
-                                </div>
-                            </> : screenState === "gameover" ?
+                                    </ul></div>
+                                <HoverButton onClick={() => setScreenState("mainmenu")} text="Back to main menu" hoverStyle={Styles.buttonHoverStyle} normalStyle={Styles.buttonStyle} />
+                            </div>
+                            : screenState === "gameover" ?
                                 <>
                                     <p style={Styles.header}>Game over</p>
                                     <br />
