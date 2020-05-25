6969fc96eb48acfc40c3b59c7c9878d80c773b83
oi <onno.invernizzi@ivention.nl>
2020-05-25 23:03:02 +0200

    Default to main menu. Whoops

--- src/UI/MainMenu.tsx
+++ src/UI/MainMenu.tsx
@@ -16,7 +16,7 @@ import { Styles } from "./Styles";
 
 export default function MainMenu(): JSX.Element {
 
-    const [screenState, setScreenState] = useState<"mainmenu" | "playing" | "about" | "gameover">("about");
+    const [screenState, setScreenState] = useState<"mainmenu" | "playing" | "about" | "gameover">("mainmenu");
     const [gameResult, setGameResult] = useState<GameResultModel>();
 
     /**
@@ -112,38 +112,37 @@ export default function MainMenu(): JSX.Element {
                             </div>
                         </> :
                         screenState === "about" ?
-                            <div style={Styles.defaultContainer}>
+                            <>
                                 <p style={Styles.header}>About me</p>
-                                <div style={{flexDirection: "column"}} >
-                                    <p style={Styles.textStyle}>
-                                        My name is Antonio Invernizzi. I've worked as a professional programmer for 20 years.<br />
-                                        <br />
+                                <p style={Styles.textStyle}>
+                                    My name is Antonio Invernizzi. I've worked as a professional programmer for 20 years.<br />
+                                    <br />
                                         Round 42 is a big reason why.<br />
-                                        <br />
+                                    <br />
                                         It was 1990 and my parents bought our first PC. A 4 Mhz 8088 XT. It shipped with 3 games:
                                         <ul>
-                                            <li>Digger</li>
-                                            <li>A pinball machine</li>
-                                            <li>Round 42</li>
-                                        </ul>
-                                    </p>
-                                    <p>
-                                        It is fair to say I was instantly addicted to Round 42 and spend many, many hours trying to beat the game often with my mother watching. <br />
+                                        <li>Digger</li>
+                                        <li>A pinball machine</li>
+                                        <li>Round 42</li>
+                                    </ul>
+                                </p>
+                                <p>
+                                    It is fair to say I was instantly addicted to Round 42 and spend many, many hours trying to beat the game often with my mother watching. <br />
                                         Good times :) <br />
                                         Course, spending hour uppon hour behind a PC made me curious what else I could do with it and... well... now I'm a programmer. <br />
                                         Though, I am not a game developer by trade I realy enjoyed the challenge of writing one.
                                     </p>
-                                    <p style={Styles.header}>Technologies used</p>
-                                    <ul>
-                                        <li>TypeScript for coding</li>
-                                        <li>WebPack for packaging</li>
-                                        <li>Redux for state</li>
-                                        <li>Immer for QoL state management.</li>
-                                        <li>React (menu only)</li>
-                                        <li>Howler for playing sounds.</li>
-                                    </ul></div>
+                                <p style={Styles.header}>Technologies used</p>
+                                <ul>
+                                    <li>TypeScript for coding</li>
+                                    <li>WebPack for packaging</li>
+                                    <li>Redux for state</li>
+                                    <li>Immer for QoL state management.</li>
+                                    <li>React (menu only)</li>
+                                    <li>Howler for playing sounds.</li>
+                                </ul>
                                 <HoverButton onClick={() => setScreenState("mainmenu")} text="Back to main menu" hoverStyle={Styles.buttonHoverStyle} normalStyle={Styles.buttonStyle} />
-                            </div>
+                            </>
                             : screenState === "gameover" ?
                                 <>
                                     <p style={Styles.header}>Game over</p>
@@ -165,7 +164,7 @@ export default function MainMenu(): JSX.Element {
                                 </>
                                 : null
             }
-        </div>
+        </div >
     );
 
     /**
