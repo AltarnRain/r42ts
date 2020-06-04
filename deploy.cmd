9bae0d4ee181bd09f02df8a10322cd510ae76191
oi <onno.invernizzi@ivention.nl>
2020-06-04 18:54:40 +0200

    Add deploy to source control.

---
+++ deploy.cmd
@@ -0,0 +1,25 @@
+@echo off
+echo cleaning folder
+del /s /q ..\altarnrain.github.io\round42
+
+echo Cleaning old sound files
+cd dist
+rd /s /q sounds
+cd.. 
+
+echo Building game
+call tsc --build
+
+echo Packing
+call webpack -p
+
+echo Copying gamefiles.
+xcopy dist ..\altarnrain.github.io\round42 /y /s
+
+echo Committing and pushing
+cd /d ..\altarnrain.github.io
+git add .
+git commit -m "Round 42 update"
+git push
+pause
+exit
